53aed3c43525d8f6f1fa15b20cb916eabcf45db4
Jesus Vite <127768350+jesusvite@users.noreply.github.com>
2026-03-06 15:56:44 -0600

    Agregué datos a las otras secciónes

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -68,3 +68,79 @@ document.querySelectorAll('.nav-header a').forEach(link => {
         }
     });
 });
+/* Añadir proyectos nuevos */
+const proyectos = [
+{
+    nombre: "Todo List",
+    imagen: "./proyectos/img/todo.png",
+    descripcion: "Aplicación web para crear, buscar, completar y eliminar tareas utilizando React.",
+    github: "https://github.com/jesusvittee/todo-list-react",
+    web: "https://jesusvittee.github.io/todo-list-react/",
+    tecnologias: ["React", "JavaScript", "CSS", "HTML", "Local Storage"]
+},
+
+{
+    nombre: "Platzi Travel",
+    imagen: "./proyectos/img/travel.png",
+    descripcion: "Página web de destinos turísticos creada utilizando TailwindCSS con diseño Mobile First.",
+    github: "https://github.com/jesusvittee/platzi-travel",
+    web: "https://jesusvittee.github.io/platzi-travel/public/",
+    tecnologias: ["TailwindCSS", "JavaScript", "HTML", "CSS"]
+},
+
+{
+    nombre: "Shopi",
+    imagen: "./proyectos/img/shopi.png",
+    descripcion: "Tienda online desarrollada con React, Vite y TailwindCSS.",
+    github: "https://github.com/jesusvittee/shopi",
+    web: "https://jesusvittee.github.io/shopi/",
+    tecnologias: ["React", "Vite", "TailwindCSS", "React Router"]
+}
+];
+const contenedor = document.getElementById("lista-proyectos");
+
+proyectos.forEach(proyecto => {
+
+    const tecnologias = proyecto.tecnologias
+        .map(tec => `<li>${tec}</li>`)
+        .join("");
+
+    const html = `
+    <div class="proyecto">
+
+        <figure class="proyecto-contenedor__img">
+            <img class="proyecto-img" src="${proyecto.imagen}" alt="${proyecto.nombre}">
+        </figure>
+
+        <div class="proyecto-informacion">
+
+            <h4>${proyecto.nombre}</h4>
+
+            <div class="proyecto-links">
+
+                <a href="${proyecto.github}" class="link-proyecto" target="_blank">
+                    Código
+                </a>
+
+                <a href="${proyecto.web}" class="link-proyecto" target="_blank">
+                    Demo
+                </a>
+
+            </div>
+
+            <p class="proyecto-texto">
+                ${proyecto.descripcion}
+            </p>
+
+            <ul>
+                ${tecnologias}
+            </ul>
+
+        </div>
+
+    </div>
+    `;
+
+    contenedor.innerHTML += html;
+
+});
